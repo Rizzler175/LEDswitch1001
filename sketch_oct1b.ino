0bfb1a0a95693ef863cfdf9a75a12ff98b3ea99d
Rizzler175 <t113ac2009@ntut.org.tw>
2025-10-01 16:10:07 +0800

    Add files via upload

---
+++ sketch_oct1b.ino
@@ -0,0 +1,20 @@
+const int pushButton = 2;
+const int ledPin = 3;
+int buttonState = 0;
+
+void setup() {
+pinMode(ledPin, OUTPUT);
+pinMode(pushButton, INPUT);
+
+}
+
+void loop() {
+buttonState = digitalRead(pushButton);
+
+if(buttonState == HIGH){
+  digitalWrite(ledPin, LOW);
+}else{
+  digitalWrite(ledPin, HIGH);
+}
+
+}
